ae6ce9e736dd347731447ac68fb468600a6ddd5b
Usman Faizyab Khan <usmanfaizyab123@gmail.com>
2025-04-30 10:11:03 +0000

    FOlder structure added

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-import app from "../src/index.js";  // Import your Express app from src/index.js
+import app from "../src/app.js";  // Import your Express app from src/index.js
 
 export default app;  // Export the app for Vercel's serverless function handler
